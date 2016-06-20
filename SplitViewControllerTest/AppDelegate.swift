5257f65854b17f2388acbfd288a3b2cc152805eb
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-06-20 07:33:17 -0400

    Test

--- SplitViewControllerTest/AppDelegate.swift
+++ SplitViewControllerTest/AppDelegate.swift
@@ -1,9 +1,5 @@
 //
 //  AppDelegate.swift
-//  SplitViewControllerTest
-//
-//  Created by 齋藤緒 on 2016/06/20.
-//  Copyright © 2016年 TestOrganization. All rights reserved.
 //
 
 import UIKit
---
+++ SplitViewControllerTest/TestTableTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TestTableTableViewController.swift
+//
+
+import UIKit
+
+class TestTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
